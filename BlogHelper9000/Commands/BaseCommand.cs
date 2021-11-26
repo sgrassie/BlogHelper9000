0657d99b0c480382f05938d453874d6325dd224f
Stuart Grassie <sgrassie@gmail.com>
2021-11-26 20:06:19 +0000

    Add base command; refactor addcommand

---
+++ BlogHelper9000/Commands/BaseCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace BlogHelper9000.Commands
+{
+    public abstract class BaseCommand<TInput> : OaktonCommand<TInput>
+    {
+        private static string _draftsPath = Path.Combine(AppContext.BaseDirectory, "_drafts");
+        private static string _postsPath = Path.Combine(AppContext.BaseDirectory, "_posts", DateTime.Now.Year.ToString());
+
+        public string DraftsPath => _draftsPath;
+        public string PostsPath => _postsPath;
+
+        public override bool Execute(TInput input)
+        {
+            return Run(input);
+        }
+
+        public abstract bool Run(TInput input);
+    }
+}
+
