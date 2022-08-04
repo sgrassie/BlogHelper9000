d2edf672bccd900389dfca113dc0b0cd9501f703
Stuart Grassie <sgrassie@gmail.com>
2022-08-04 22:03:01 +0100

    Update YamlDeserialiser.cs
    Refactor this to make the inline methods explicit private methods

--- BlogHelper9000/YamlParsing/YamlDeserialiser.cs
+++ BlogHelper9000/YamlParsing/YamlDeserialiser.cs
@@ -25,7 +25,7 @@ public sealed class YamlDeserialiser : SerialiserBase
 
             yamlBlock.Add(line);
         }
-            
+
         return ParseYamlHeader(yamlBlock);
     }
 
@@ -50,90 +50,90 @@ public sealed class YamlDeserialiser : SerialiserBase
         }
 
         return ToYamlHeader(parsedHeaderProperties, extraHeaderProperties);
+    }
 
-        (string property, string value) ParseHeaderTag(string tag)
+    private YamlHeader ToYamlHeader(Dictionary<string, object> source, Dictionary<string, string> extras)
+    {
+        var header = new YamlHeader();
+        var yamlHeaderType = header.GetType();
+
+        foreach (var item in source)
         {
-            tag = tag.Trim();
-            var index = tag.IndexOf(':');
-            var property = tag.Substring(0, index);
-            var value = tag.Substring(index+1).Trim();
-            return (property, value);
+            try
+            {
+                var key = item.Key.Replace("_", string.Empty);
+                var property = GetPropertyInfo(yamlHeaderType, key);
+                if (property?.PropertyType == typeof(string))
+                {
+                    property.SetValue(header, (string)item.Value, null);
+                }
+
+                if (property?.PropertyType == typeof(bool?))
+                {
+                    var value = bool.Parse((string)item.Value);
+                    property.SetValue(header, value, null);
+                }
+
+                if (property?.PropertyType == typeof(DateTime?))
+                {
+                    var value = (string)item.Value;
+                    var date = value is "draft" or "true" or "false"
+                        ? DateTime.MinValue
+                        : DateTime.ParseExact((string)item.Value, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                    property.SetValue(header, date, null);
+                }
+
+                if (property?.PropertyType == typeof(List<string>))
+                {
+                    var list = ((string)item.Value)
+                        .Replace("[", string.Empty)
+                        .Replace("]", string.Empty)
+                        .Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                        .ToList();
+                    property.SetValue(header, list, null);
+                }
+
+                header.Extras = extras;
+            }
+            catch (Exception e)
+            {
+                var message = $"Error deserialising property '{item.Key}'";
+                throw new YamlConvertException(message, e);
+            }
         }
 
-        PropertyInfo? GetPropertyInfo(Type type, string propertyName)
+        return header;
+    }
+
+    private static PropertyInfo? GetPropertyInfo(Type type, string propertyName)
+    {
+        var property = type.GetProperty(propertyName,
+            BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+        if (property is not null) return property;
+
+        foreach (var prop in type.GetProperties())
         {
-            var property = type.GetProperty(propertyName,
-                BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            var attribute = prop.GetCustomAttribute<YamlNameAttribute>();
 
-            if (property is not null) return property;
-
-            foreach (var prop in type.GetProperties())
+            if (attribute is not null)
             {
-                var attribute = prop.GetCustomAttribute<YamlNameAttribute>();
-
-                if (attribute is not null)
+                if (attribute.Name == propertyName)
                 {
-                    if (attribute.Name == propertyName)
-                    {
-                        return prop;
-                    }
+                    return prop;
                 }
             }
-
-            return null;
         }
 
-        YamlHeader ToYamlHeader(Dictionary<string, object> source, Dictionary<string, string> extras)
-        {
-            var header = new YamlHeader();
-            var yamlHeaderType = header.GetType();
+        return null;
+    }
 
-            foreach (var item in source)
-            {
-                try
-                {
-                    var key = item.Key.Replace("_", string.Empty);
-                    var property = GetPropertyInfo(yamlHeaderType, key);
-                    if (property?.PropertyType == typeof(string))
-                    {
-                        property.SetValue(header, (string)item.Value, null);
-                    }
-
-                    if (property?.PropertyType == typeof(bool?))
-                    {
-                        var value = bool.Parse((string)item.Value);
-                        property.SetValue(header, value, null);
-                    }
-
-                    if (property?.PropertyType == typeof(DateTime?))
-                    {
-                        var value = (string)item.Value;
-                        var date = value is "draft" or "true" or "false"
-                            ? DateTime.MinValue
-                            : DateTime.ParseExact((string)item.Value, "dd/MM/yyyy", CultureInfo.InvariantCulture);
-                        property.SetValue(header, date, null);
-                    }
-
-                    if (property?.PropertyType == typeof(List<string>))
-                    {
-                        var list = ((string)item.Value)
-                            .Replace("[", string.Empty)
-                            .Replace("]", string.Empty)
-                            .Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
-                            .ToList();
-                        property.SetValue(header, list, null);
-                    }
-
-                    header.Extras = extras;
-                }
-                catch (Exception e)
-                {
-                    var message = $"Error deserialising property '{item.Key}'";
-                    throw new YamlConvertException(message, e);
-                }
-            }
-
-            return header;
-        }
+    private static (string property, string value) ParseHeaderTag(string tag)
+    {
+        tag = tag.Trim();
+        var index = tag.IndexOf(':');
+        var property = tag.Substring(0, index);
+        var value = tag.Substring(index + 1).Trim();
+        return (property, value);
     }
 }
