86a7be40a2a9bf6d1a1eea74e63ceb0045f759bf
Stuart Grassie <sgrassie@gmail.com>
2024-02-22 22:07:35 +0000

    Tidy up publish handler
    Decorating the TryFindPublishablePost out parameter with the [NotNullWhen] attribute lets us remove an unnecessary if, because there is no need to check if the markdown file is null.

--- BlogHelper9000/Handlers/PublishCommandHandler.cs
+++ BlogHelper9000/Handlers/PublishCommandHandler.cs
@@ -1,69 +1,59 @@
+using System.Diagnostics.CodeAnalysis;
 using BlogHelper9000.Helpers;
 
 namespace BlogHelper9000.Handlers;
 
-public class PublishCommandHandler
+public class PublishCommandHandler(ILogger logger, PostManager postManager)
 {
-    private readonly ILogger _logger;
-    private readonly PostManager _postManager;
-
-    public PublishCommandHandler(ILogger logger, PostManager postManager)
-    {
-        _logger = logger;
-        _postManager = postManager;
-    }
-
     public void Execute(string post)
     {
         if (TryFindPublishablePost(post, out var postMarkdown))
         {
-            if (postMarkdown != null)
+            logger.LogDebug("Found a publishable post at {PostFilePath}", postMarkdown.FilePath);
+
+            var currentPath = postMarkdown.FilePath;
+            postMarkdown.Metadata.IsPublished = true;
+            postMarkdown.Metadata.PublishedOn = DateTime.Now;
+            postManager.Markdown.UpdateFile(postMarkdown);
+
+            var publishedFilename = $"{DateTime.Now:yyyy-MM-dd}-{post}";
+            var targetFolder = postManager.FileSystem.Path.Combine(postManager.Posts, $"{DateTime.Now:yyyy}");
+
+            if (!postManager.FileSystem.Directory.Exists(targetFolder))
             {
-                _logger.LogDebug("Found a publishable post at {PostFilePath}", postMarkdown.FilePath);
-                
-                var currentPath = postMarkdown.FilePath;
-                postMarkdown.Metadata.IsPublished = true;
-                postMarkdown.Metadata.PublishedOn = DateTime.Now;
-                _postManager.Markdown.UpdateFile(postMarkdown);
-
-                var publishedFilename = $"{DateTime.Now:yyyy-MM-dd}-{post}";
-                var targetFolder = _postManager.FileSystem.Path.Combine(_postManager.Posts, $"{DateTime.Now:yyyy}");
-
-                if (!_postManager.FileSystem.Directory.Exists(targetFolder))
-                {
-                    _logger.LogDebug("Creating folder for post at {PostPublishTarget}", targetFolder);
-                    _postManager.FileSystem.Directory.CreateDirectory(targetFolder);
-                }
-                
-                var replacementPath = _postManager.FileSystem.Path.Combine(targetFolder, publishedFilename);
-                
-                //ConsoleWriter.Write("Publishing {0} to {1}", publishedFilename, targetFolder);
-                _postManager.FileSystem.File.Move(currentPath, replacementPath);
-                _postManager.FileSystem.File.Delete(currentPath);
-                //await Command.RunAsync("git", "add --all", input.BaseDirectoryFlag, true);
-                //ConsoleWriter.Write("Published file added to git index. Don't forget to commit and push to remote.");
+                logger.LogDebug("Creating folder for post at {PostPublishTarget}", targetFolder);
+                postManager.FileSystem.Directory.CreateDirectory(targetFolder);
             }
+
+            var replacementPath = postManager.FileSystem.Path.Combine(targetFolder, publishedFilename);
+
+            logger.LogInformation("Publishing {PublishedFileName} to {TargetFolder}", publishedFilename, targetFolder);
+            postManager.FileSystem.File.Move(currentPath, replacementPath);
+            postManager.FileSystem.File.Delete(currentPath);
+            
+            //await Command.RunAsync("git", "add --all", input.BaseDirectoryFlag, true);
+            //ConsoleWriter.Write("Published file added to git index. Don't forget to commit and push to remote.");
         }
         else
         {
-            _logger.LogError("Could not find {Post} to publish", post);
+            logger.LogError("Could not find {Post} to publish", post);
         }
     }
 
-    private bool TryFindPublishablePost(string title, out MarkdownFile? markdownFile)
+    private bool TryFindPublishablePost(string title, [NotNullWhen(returnValue: true)]out MarkdownFile? markdownFile)
     {
-        var potentialDraftPath = _postManager.CreateDraftPath(title);
-        var potentialPostsPath = _postManager.CreatePostPath(title);
+        var potentialDraftPath = postManager.CreateDraftPath(title);
+        var potentialPostsPath = postManager.CreatePostPath(title);
         
-        if (_postManager.FileSystem.File.Exists(potentialDraftPath))
+        if (postManager.FileSystem.File.Exists(potentialDraftPath))
         {
-            markdownFile = _postManager.Markdown.LoadFile(potentialDraftPath);
+            markdownFile = postManager.Markdown.LoadFile(potentialDraftPath);
             return true;
         }
 
-        if (_postManager.FileSystem.File.Exists(potentialPostsPath))
+        if (postManager.FileSystem.File.Exists(potentialPostsPath))
         {
-            markdownFile = _postManager.Markdown.LoadFile(potentialPostsPath);
+            markdownFile = postManager.Markdown.LoadFile(potentialPostsPath);
             return true;
         }
 
