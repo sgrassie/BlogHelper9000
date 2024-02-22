a26bc716a7c24f681d02681f0eabf15c0d90ac63
Stuart Grassie <sgrassie@gmail.com>
2024-02-22 21:37:51 +0000

    Update filename to match class

--- BlogHelper9000/GlobalUsings.cs
+++ BlogHelper9000/GlobalUsings.cs
@@ -2,3 +2,5 @@ global using Spectre.Console;
 global using SimpleExec;
 global using System.CommandLine;
 global using System.IO.Abstractions;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Logging.Abstractions;
