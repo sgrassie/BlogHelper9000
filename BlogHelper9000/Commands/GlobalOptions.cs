93f6e5b6d375a871f59ba118dfbb79c9cfa0cdc2
Stuart Grassie <sgrassie@gmail.com>
2024-02-22 21:37:14 +0000

    Add verbosity option

--- BlogHelper9000/Commands/GlobalOptions.cs
+++ BlogHelper9000/Commands/GlobalOptions.cs
@@ -5,4 +5,9 @@ public static class GlobalOptions
     public static readonly Option<string> BaseDirectoryOption = new(
             new[] {"--base-directory", "-b"},
             description: "The base directory of the blog.");
+
+    public static readonly Option<LogLevel> VerbosityOption = new(
+        new[] { "--verbosity", "-v" },
+        description: "The log level to output.",
+        getDefaultValue: () => LogLevel.None);
 }
