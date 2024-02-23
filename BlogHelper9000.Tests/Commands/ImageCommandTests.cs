137a5d24a9e5298c377101b5ec33b08aa0ff4e79
Stuart Grassie <sgrassie@gmail.com>
2024-02-23 23:24:52 +0000

    Build out command line for image command
    Design the command line in the unit tests.

--- BlogHelper9000.Tests/Commands/ImageCommandTests.cs
+++ BlogHelper9000.Tests/Commands/ImageCommandTests.cs
@@ -8,7 +8,7 @@ namespace BlogHelper9000.Tests.Commands;
 public class ImageCommandTests
 {
     [Fact]
-    public async Task Should_Output_Help()
+    public async Task Should_Output_Help_ForMainImageCommand()
     {
         var expectedHelp = new List<string>
         {
@@ -21,7 +21,8 @@ public class ImageCommandTests
             "--version                              Show version information",
             "-?, -h, --help                         Show help and usage information",
             "Commands:",
-            "add <post>  Add an image to a post",
+            "add <post> <query>  Add an image to a post [default: programming]",
+            "update              Update images in posts"
         };
         var console = new TestConsole();
         var fileSystem = new JekyllBlogFilesystemBuilder().BuildFileSystem();
@@ -29,6 +30,34 @@ public class ImageCommandTests
         command.AddOption(GlobalOptions.BaseDirectoryOption);
 
         await command.InvokeAsync("image -h", console);
+
+        var lines = console.AsLines();
+
+        lines.Should().ContainInOrder(expectedHelp);
+    }
+
+    [Fact]
+    public async Task Should_Output_Help_ForImageUpdateSubCommand()
+    {
+        var expectedHelp = new[]
+        {
+            "Description:", 
+            "Update images in posts", 
+            "Usage:", 
+            "image update [command] [options]", 
+            "Options:", 
+            "-b, --branding <branding>  Optionally provide author branding. [default: /assets/images/branding_logo.png]",
+            "-?, -h, --help             Show help and usage information", 
+            "Commands:", 
+            "post <post> <query>  Updates the image in a specific post [default: programming]",
+            "all <query>          Updates all images in all posts [default: programming]"
+        };
+        var console = new TestConsole();
+        var fileSystem = new JekyllBlogFilesystemBuilder().BuildFileSystem();
+        var command = new ImageCommand(fileSystem);
+        command.AddOption(GlobalOptions.BaseDirectoryOption);
+
+        await command.InvokeAsync("image update -h", console);
 
         var lines = console.AsLines();
 
--- BlogHelper9000/Commands/ImageCommand.cs
+++ BlogHelper9000/Commands/ImageCommand.cs
@@ -10,19 +10,61 @@ internal sealed class ImageCommand : Command
         : base("image", "Manipulate images in blog posts")
     {
         AddCommand(new AddSubCommand(fileSystem));
+        AddCommand(new UpdateSubCommand(fileSystem));
     }
 
     private static class ImageCommandSharedOptions
     {
-        public static Argument<string> PostArg = new(
+        public static readonly Argument<string> PostArg = new(
             "post", "The post to update the image for.");
+
+        public static readonly Argument<string> QueryArg = new(
+            "query", () => "programming", "The image query."
+        );
+
+        public static readonly Option<string> AuthorBrandingOption = new(
+            new []{"--branding", "-b"}, description: "Optionally provide author branding.",
+            getDefaultValue: () => "/assets/images/branding_logo.png"
+        );
     }
     
     private class AddSubCommand : Command
     {
-        public AddSubCommand(IFileSystem fileSystem) : base("add", "Add an image to a post")
+        public AddSubCommand(IFileSystem fileSystem) 
+            : base("add", "Add an image to a post")
         {
             AddArgument(ImageCommandSharedOptions.PostArg);
+            AddArgument(ImageCommandSharedOptions.QueryArg);
+        }
+    }
+
+    private class UpdateSubCommand : Command
+    {
+        public UpdateSubCommand(IFileSystem fileSystem) 
+            : base("update", "Update images in posts")
+        {
+            AddCommand(new UpdatePostSubCommand(fileSystem));
+            AddCommand(new UpdateAllSubCommand(fileSystem));
+            AddGlobalOption(ImageCommandSharedOptions.AuthorBrandingOption);
+        }
+
+        private class UpdatePostSubCommand : Command
+        {
+            public UpdatePostSubCommand(IFileSystem fileSystem)
+                : base("post", "Updates the image in a specific post")
+            {
+                AddArgument(ImageCommandSharedOptions.PostArg);
+                AddArgument(ImageCommandSharedOptions.QueryArg);
+            }
+        }
+
+        private class UpdateAllSubCommand : Command
+        {
+            public UpdateAllSubCommand(IFileSystem fileSystem)
+                : base("all", "Updates all images in all posts")
+            {
+                AddArgument(ImageCommandSharedOptions.QueryArg);
+            }
         }
     }
 }
