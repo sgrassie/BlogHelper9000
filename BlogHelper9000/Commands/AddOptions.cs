c523941be98feb67227ba4cdd54178d28b68f7d9
Stuart Grassie <sgrassie@gmail.com>
2025-11-02 11:23:02 +0000

    Remove AddOptions record from Commands
    Deleted the AddOptions record from the BlogHelper9000.Commands namespace. It's obsolete.

--- BlogHelper9000/Commands/AddOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace BlogHelper9000.Commands;
-
-public record AddOptions(
-    string Title, 
-    string[] Tags, 
-    string? FeaturedImage, 
-    bool IsDraft, 
-    bool IsFeatured, 
-    bool IsHidden);
